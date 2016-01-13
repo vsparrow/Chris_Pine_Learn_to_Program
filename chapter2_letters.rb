5fa944aaf9990d3147653c31e68e939217bb1e3f
Mohammad Khan <khan@vsparrow.com>
2016-01-13 14:41:41 -0500

    Added chapter2 work

---
+++ chapter2_letters.rb
@@ -0,0 +1,30 @@
+=begin
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye'
+
+puts 'I like'+'apple pie.'
+puts 'I like '+'apple pie.'
+puts 'I like' + ' apple pie.'
+
+puts 'blink ' * 4
+
+puts  12 +  12
+puts '12'+ '12'
+puts '12 +  12'
+=end
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+
+#puts '12' + 12  # will not work can't really add a number to a string,
+#puts '2' * '5'  # no  multiply a string by another string.
+
+
+# The only things the backslash escapes, though, are the apostrophe and the backslash itself.
+puts 'You\'re swell!'	#escaping apostrophe
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'	#does not escape d so nothing happens
