3934a4637e93b74cfbe1e7e09068f3fbb96194d0
Mohammad Khan <khan@vsparrow.com>
2016-01-13 13:55:33 -0500

    Adding calulator.rb edits

---
+++ calculator.rb
@@ -0,0 +1,41 @@
+=begin
+puts 1+2
+puts 1.0 +2.0
+puts 2.0 + 3.0
+puts 5.0-8.0
+puts 9.0/2.0
+=end
+
+=begin
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+puts 5 *(12-8) + -15
+puts 98 +(59872/(13*8)) * -52
+=end
+
+# how many hours are in a year? 
+print "Hours in a year: "
+print 365 * 24
+
+# how many minutes are in a decade? 
+print "\nMinutes in a decade: "
+print 10*365*24*60
+
+ # how many seconds old are you? 1000 years old, crazy man
+ print "\nI am how old in seconds? : Estimate  :"
+ print 365*24*60*60*1000
+
+  # how many seconds old are you? 1000 years old
+ print "\nI am how old in seconds? : NearActual:"
+ print 365*24*60*60*1000 + (31+30+31+30+31+13+14)*24*60*60 + (13*60+29)*60
+
+ #how many chocolates do you hope to eat in your lifetime?
+ print "\nHow man chocolates do you wish to eat in your lifetime? in lbs:"
+ print 1000*365
+
+ #If I am 1235 million seconds old, how old am I?
+ print "\nIf you are 1235 million seconds old , in years you are: "
+ print  (1235000000/(60*60*24*365))
