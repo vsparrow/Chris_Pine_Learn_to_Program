235009585e92bfeed6556a0708605decf4e3784d
vsparrow <khan@vsparrow.com>
2016-01-26 23:02:47 -0500

    Add refactor of num to alpha

--- chapter8_writing_your_own_methods.rb
+++ chapter8_writing_your_own_methods.rb
@@ -207,7 +207,7 @@ puts wetsBed
 
 =end
 #*************************************************************************
-
+=begin
 def englishNumber number # we only want 0-100
 	if number < 0
 		return 'Please enter a number 0 or greater'
@@ -322,3 +322,83 @@ puts englishNumber( 32)
 puts englishNumber( 88)
 puts englishNumber( 99)
 puts englishNumber(100)
+
+=end
+
+#********************************************************
+#refactor num to alpha
+
+def englishNumber number 
+	if number < 0
+		return 'Please enter a non-negative number'
+	end
+	if number == 0
+		return 'zero'
+	end
+	# no more special cases
+
+
+	numString = ''
+	onesPlace = ['one','two','three','four','five','six','seven','eight','nine']
+	tensPlace = ['ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety']
+	teenagers = ['eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+
+	# left is how much of the number if left to write
+	# write is 	the part we are working on 
+
+	left = number
+	write = left / 100 # how many hundreds to write
+	left = left - write * 100 # subtract hundreds
+
+	if write > 0 # 1st
+		hundreds = englishNumber write
+		numString = numString + hundreds + ' hundred'
+		
+		if left > 0
+			numString = numString + ' '
+		end # if left > 0	
+	end #if write > 0 #1st
+
+
+	write = left / 10 # how many tens?
+	left = left - write * 10
+
+	if write > 0 # two
+		if write == 1 and left > 0
+			numString = numString + teenagers[left-1]
+			left = 0
+		else
+			numString = numString + tensPlace[write-1]
+		end # if wtite == 1 and left > 0
+
+		if left > 0
+			numString = numString + '-'
+		end
+	end # if write > 0 two
+
+	write = left
+	left = 0
+
+	if write > 0
+		numString = numString + onesPlace[write-1]
+	end
+
+	numString # return
+end # def
+
+
+
+puts englishNumber(  0)
+puts englishNumber(  9)
+puts englishNumber( 10)
+puts englishNumber( 11)
+puts englishNumber( 17)
+puts englishNumber( 32)
+puts englishNumber( 88)
+puts englishNumber( 99)
+puts englishNumber(100)
+puts englishNumber(101)
+puts englishNumber(234)
+puts englishNumber(3211)
+puts englishNumber(999999)
+puts englishNumber(1000000000000)
